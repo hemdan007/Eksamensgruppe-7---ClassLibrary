527de1f6e29cc09da23e0f358a5e9450a2d6972d
Kevin Abedinpour <kea003@edu.zealand.dk>
2025-05-27 02:30:21 +0200

    Added comments

--- Repositories/BookingRepo.cs
+++ Repositories/BookingRepo.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 
-
+//BY KEVIN
 
 namespace Eksamensgruppe_7___ClassLibrary.Repositories
 {
--- Repositories/IAnimalRepo.cs
+++ Repositories/IAnimalRepo.cs
@@ -2,14 +2,15 @@
 using System.Text.Json;
 using Eksamensgruppe_7___ClassLibrary.Models;
 
+//BY KEVIN
 namespace Eksamensgruppe_7___ClassLibrary.Repositories
 {
     public interface IAnimalRepo
     {
-        List<Animal> GetAll();
-        Animal GetById(int id);
-        void Add(Animal animal);
-        void Update(Animal animal);
-        void Remove(int id);
+        List<Animal> GetAll();                      // Returns a list of all animals
+        Animal GetById(int id);                     // Finds and returns animal by ID
+        void Add(Animal animal);                    // Adds a new animal to the collection
+        void Update(Animal animal);                 // Updates the data of an existing animal
+        void Remove(int id);                        // Removes animal by ID
     }
 }
